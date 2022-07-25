8d44c072faa2b07bfb1f3c66714bf9def5ccd6c2
Clara Lima <claralima601@gmail.com>
2022-07-25 11:11:00 -0300

    Resolução dos exercícios
    estudo da biblioteca turtle.

--- exercicio005.py
+++ exercicio005.py
@@ -8,14 +8,21 @@ Exercícios
 """
 
 import turtle
+import random # importar biblioteca 'random' para gerar números aleatórios
 
 turtle = turtle.Turtle()
-turtle.color ('red')
+colors= ('#6A5ACD', '#00BFFF','#87CEEB', '#48D1CC','#F08080')
 
+turtle.shape('turtle')
 for _ in [1, 2, 3]:
+    color = random.choice(colors) # a cor será escolhida aleatoriamente pela biblioteca 'random'
+    turtle.color(color) 
     turtle.forward(100)
     turtle.right(120)
 
+turtle.shape('circle')
 for _ in [1, 2, 3, 4]:
-  turtle.forward(100)
+  color = random.choice(colors)
+  turtle.color(color)
+  turtle.forward(200)
   turtle.right(90)
