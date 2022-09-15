8f0c80a47cd520c099a0836fc7ee98ad76d0cd11
Clara Lima <claralima601@gmail.com>
2022-09-15 16:15:08 -0300

    Atualização

--- Criando_formas_geometricas.py
+++ Criando_formas_geometricas.py
@@ -1,37 +1,141 @@
+from msilib import change_sequence
 import turtle
-
+from turtle import onscreenclick, mainloop, onkey, listen
 turtle = turtle.Turtle()
 
-def square():
+def square(start, end):
+    turtle.penup()
+    turtle.goto(start[0], start[1])
+    turtle.pendown()
+
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+
+    width = max(diff_x, diff_y)
+
     for _ in range(4):
-        turtle.forward(100)
+        turtle.forward(width)
         turtle.left(90)
-    
-def triangle():
+
+
+
+def triangle(start, end):
+
+    turtle.penup()
+    turtle.goto(start[0], start[1])
+    turtle.pendown()
+
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+
+    width = max(diff_x, diff_y)
+
+
     for _ in range(3):
         turtle.forward(100)
         turtle.left(120)
 
-def retangle():
+
+def to_square():
+    global shape
+    shape = square
+
+def to_rectangle():
+    global shape
+    shape = rectangle
+
+def to_triangle():
+    global shape
+    shape = triangle
+
+def to_line():
+    global shape
+    shape = line
+
+def to_star():
+    global shape
+    shape = star
+
+def rectangle(start, end):
+
+    turtle.penup()
+    turtle.goto(start[0], start[1])
+    turtle.pendown()
+
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+
+    width = max(diff_x, diff_y)
+
     for _ in range(4):
         turtle.forward(200)
         turtle.left(90)
 
-def line():
+
+
+def line(start, end):
+    turtle.penup()
+    turtle.goto(start[0], start[1])
+    turtle.pendown()
+
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+
+    width = max(diff_x, diff_y)
     turtle.forward(100)
 
-turtle.color('blue')
 
-def star():
+def star(start, end):
+    turtle.penup()
+    turtle.goto(start[0], start[1])
+    turtle.pendown()
+
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+
+    width = max(diff_x, diff_y)
+
     for _ in range(20):
         turtle.left(90)
         turtle.forward(100)
         turtle.left(10)
         turtle.backward(100)
         turtle.left(10)
-       
 
-star()
+
+def sol():
+    for _ in range():
+        turtle.circle()
+
+#Estado
+start = None
+shape= square
+   
+
+def tap(x, y):
+    global start
+    if start is None:
+        start = x, y
+
+    else:
+        end = x, y
+        shape(start, end)
+        start = None
+
+def change_shape(next_shape):
+    global shape
+    shape = next_shape
+
+onkey(lambda: change_shape(square), 's')
+onkey(lambda: change_shape(rectangle), 'r')
+onkey(lambda: change_shape(triangle), 't')
+onkey(lambda: change_shape(line), 'l')
+onkey(lambda: change_shape(star), 'S')
+
+listen()
+onscreenclick(tap)
+mainloop()
+
 
 
 
