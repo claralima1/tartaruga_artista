dee222282cc4304f8177660089d53789c37c577e
Clara Lima <86741485+claralima1@users.noreply.github.com>
2022-11-16 10:55:29 -0300

    Atualização

--- Paint.py
+++ Paint.py
@@ -2,34 +2,90 @@ from turtle import *
 
 from freegames import vector
 
-class Caneta():
-    def __init__(self, start_x, start_y):
-        self.position = goto(start_x, start_y)
-    
+class StatePen():
+    def __init__(self, position_x, position_y):
+        self.position = goto(position_x, position_y)
+
     def line(start, end):
         up()
         goto(start.x, start.y)
-        down()
         goto(end.x, end.y)
 
-        
+    def tap( x, y):
+        state = {'start': None, 'shape': None}
+        start = state['start']
+        state['start'] = None
 
-    def tap(self, x, y):
-
-        self.state = {'start': None, 'shape': line}
-        self.start = self.state['start']
-        self.state['start'] = None
-
-        if self.start is None:
-            self.state['start'] = vector(x, y)
+        if start is None:
+            state['start'] = vector(x, y)
         else:
-            self.shape = self.state['shape']
-            self.end = vector(x, y)
-            shape(self.start, self.end)
-    onscreenclick(tap) 
+            shape = state['shape']
+            end = vector(x, y)
+            shape(start, end)
+    onscreenclick(tap)
+
+    def store(key, value):
+        state[key] = value
+
+class Shapes():
+    def __init__(self, position_x, position_y):
+        self.position = goto(position_x, position_y)
+
+    def square(start, end):
+        up()
+        goto(start.x, start.y)
+        down()
+
+        begin_fill()
+
+        for count in range(4):
+            forward(end.x - start.x)
+            left(90)
+
+        end_fill()
+
+    def circle(start, end):
+        up()
+        goto(start.x, start.y)
+        goto(end.x, end.y)
+        down()
+
+        begin_fill()
+
+        for count in range(360):
+            forward(end.x - start.x)
+            left(1)
+
+        end_fill() 
+
+    def rectangle(start, end):
+        up()
+        goto(start.x, start.y)
+        goto(end.x, end.y)
+        down()
+
+        begin_fill()
+
+        for count in range(4):
+            forward(end.x - start.x)
+            forward(end.x - start.x)
+            left(90)
+
+        end_fill() 
     
+    def triangle(start, end):
+        up()
+        goto(start.x, start.y)
+        goto(end.x, end.y)
+        down()
 
+        begin_fill()
 
+        for count in range(3):
+            forward(end.x - start.x)
+            left(120)
+
+        end_fill() 
 
 setup(420, 420, 370, 0)
      
